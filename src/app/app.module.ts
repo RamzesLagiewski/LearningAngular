23a9751e24c9b2b02e6e90798c78e07550848fdb
RamzesLagiewski <ramzes.lagiewski@interia.pl>
2018-04-17 19:09:08 +0200

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-
-import { AppComponent } from './app.component';
-
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { provideRoutes, ROUTES} from "@angular/router";
+import { DataService } from './data.service'; 
+import { FormsModule } from "@angular/forms";
+import { AppComponent } from "./app.component";
+import { AppRoutingModule } from "./app-routing.module";
+import { HomeComponent } from "./home/home.component";
+import { AboutComponent } from "./about/about.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { Routes, RouterModule } from '@angular/router';
+import { RouterLink } from '@angular/router';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
+  declarations: [AppComponent, HomeComponent, AboutComponent],
+  imports: [BrowserModule, FormsModule, BrowserAnimationsModule, RouterModule],
+  providers: [DataService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
